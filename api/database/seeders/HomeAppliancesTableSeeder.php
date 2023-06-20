4c0dab970003579cac3af57989ed19a75a5b45d8
Ricardo Arantes <rick.hc3@gmail.com>
2023-06-20 02:27:13 -0300

    changed HomeApplianceSeeder

--- api/database/seeders/HomeAppliancesTableSeeder.php
+++ api/database/seeders/HomeAppliancesTableSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 use App\Models\HomeAppliance;
@@ -18,18 +17,117 @@ class HomeAppliancesTableSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        $voltages = [110, 220];
+        $nomesEletrodomesticos = [
+            'Geladeira',
+            'Fogão',
+            'Máquina de Lavar',
+            'Micro-ondas',
+            'Liquidificador',
+            'Aspirador de Pó',
+            'Freezer',
+            'Secadora de Roupas',
+            'Lava-Louças',
+            'Forno Elétrico',
+            'Cafeteira',
+            'Batedeira',
+            'Fritadeira Elétrica',
+            'Panela Elétrica',
+            'Sanduicheira',
+            'Chaleira Elétrica',
+            'Multiprocessador',
+            'Ventilador',
+            'Ar-Condicionado',
+            'Depurador de Ar',
+            'Exaustor',
+            'Aquecedor',
+            'Umidificador de Ar',
+            'Ferro de Passar',
+            'Torradeira',
+            'Grill',
+            'Espremedor de Frutas',
+            'Mixer',
+            'Yogurteira',
+            'Batedeira Planetária',
+            'Máquina de Costura',
+            'Enceradeira',
+            'Máquina de Café Expresso',
+            'Churrasqueira Elétrica',
+            'Máquina de Gelo',
+            'Bebedouro',
+            'Desumidificador',
+            'Purificador de Água',
+            'Esquentador',
+            'Escova Alisadora',
+            'Robô Aspirador',
+            'Climatizador de Ar',
+            'Filtro de Água',
+            'Afiador de Facas',
+            'Triturador de Alimentos',
+            'Faca Elétrica',
+            'Amaciador de Carne',
+            'Máquina de Crepe',
+            'Crepeira',
+            'Fogão Elétrico',
+            'Grill Elétrico',
+            'Lixeira Automática',
+            'Liquidificador Portátil',
+            'Balança de Cozinha',
+            'Máquina de Cerveja',
+            'Aquecedor de Água',
+            'Air Fryer',
+            'Máquina de Pão',
+            'Processador de Alimentos',
+            'Máquina de Popcorn',
+            'Máquina de Sorvete',
+            'Desidratador de Alimentos',
+            'Moedor de Café',
+            'Espremedor de Alho',
+            'Fondue Elétrico',
+            'Churrasqueira a Gás',
+            'Aquecedor de Ambiente',
+            'Liquidificador Profissional',
+            'Cortador de Legumes',
+            'Balança Digital',
+            'Yogurteira Elétrica',
+            'Máquina de Suco',
+            'Máquina de Popcakes',
+            'Máquina de Raspadinha',
+            'Climatizador Portátil',
+            'Ferro a Vapor',
+            'Máquina de Biscoitos',
+            'Prensa de Hambúrguer',
+            'Grelhador Elétrico',
+            'Lavadora de Alta Pressão',
+            'Descascador de Legumes',
+            'Espremedor de Cana',
+            'Cortador de Frios',
+            'Picadora de Carne',
+            'Secador de Cabelo',
+            'Carrinho de Bebê Elétrico',
+            'Hidromassagem Portátil',
+            'Panela de Pressão Elétrica',
+            'Máquina de Cortar Cabelo',
+            'Escova de Dentes Elétrica',
+            'Climatizador de Vinho',
+        ];
 
+
+        $voltages = [110, 220];
         $brands = ['Electrolux', 'Brastemp', 'Fischer', 'Samsung', 'LG'];
 
         $quantityRecords = $this->command->ask('How many records do you want to insert?', 50);
 
-        foreach (range(1, $quantityRecords) as $index) {
+        for ($i = 1; $i <= $quantityRecords; $i++) {
+            $nome = $faker->randomElement($nomesEletrodomesticos);
+            $descricao = $faker->sentence;
+            $voltagem = $faker->randomElement($voltages);
+            $marca = $faker->randomElement($brands);
+
             HomeAppliance::create([
-                'name' => $faker->word,
-                'description' => $faker->sentence,
-                'voltage' => $faker->randomElement($voltages),
-                'brand' => $faker->randomElement($brands),
+                'name' => $nome,
+                'description' => $descricao,
+                'voltage' => $voltagem,
+                'brand' => $marca,
             ]);
         }
     }
